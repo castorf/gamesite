9d0d16e6764985b97720526de9f4b7be55bc81f1
Meeperdude <torin@egocast.org>
2019-07-02 18:19:36 -0700

    Update map-locator.js

--- map-locator.js
+++ map-locator.js
@@ -1,23 +1,27 @@
  console.log(Object.keys(locations).length)
 // console.log(data.toString('utf8'))
-var collected = Object.entries(locations)
-var keyword = document.getElementById('keyword').value
-var searchX = document.getElementById('x').value
-var searchY = document.getElementById('y').value
-var size = document.getElementById('size').value
-for (var y = 0; y < collected.length; y++) {
-  var objecter= collected[y][1]
-  if (objecter.display_name.search(keyword) > -1) {
-    var yabloc = objecter.locations
-    var counteer = false
-    for (var x = 0; x < yabloc.length; x++) {
-      if (searchX < yabloc[x][0] && searchX + size > yabloc[x][0] && searchY < yabloc[x][1] && searchY + size > yabloc[x][1]) {
-        if (counteer == false) {
-          console.log(objecter.display_name)
+var searchButton = document.getElementById('searchButton');
+var find = function() {
+  var collected = Object.entries(locations)
+  var keyword = document.getElementById('keyword').value
+  var searchX = document.getElementById('x').value
+  var searchY = document.getElementById('y').value
+  var size = document.getElementById('size').value
+  for (var y = 0; y < collected.length; y++) {
+    var objecter= collected[y][1]
+    if (objecter.display_name.search(keyword) > -1) {
+      var yabloc = objecter.locations
+      var counteer = false
+      for (var x = 0; x < yabloc.length; x++) {
+        if (searchX < yabloc[x][0] && searchX + size > yabloc[x][0] && searchY < yabloc[x][1] && searchY + size > yabloc[x][1]) {
+          if (counteer == false) {
+            console.log(objecter.display_name)
+          }
+          console.log(yabloc[x]);
+          counteer = true
         }
-        console.log(yabloc[x]);
-        counteer = true
       }
     }
   }
-}
+};
+searchButton.addEventListener('click', find);
