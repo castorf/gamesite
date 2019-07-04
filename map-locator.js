5f29a2c31f8541ba41fa8e44513605c23661000f
Dialgras <torin@egocast.org>
2019-07-03 21:26:50 -0700

    Update map-locator.js

--- map-locator.js
+++ map-locator.js
@@ -10,31 +10,24 @@ var onclick = function() {
   var searchX = document.getElementById('x').value
   var searchY = document.getElementById('y').value
   var size = document.getElementById('size').value
+  results.innerHTML = ''
   find(searchX, searchY, size, keyword);
 }
 find = function(searchX, searchY, size, keyword) {
-  console.log('x: '+searchX)
-  console.log('y: '+searchY)
-  console.log('size: '+size)
-  console.log('keyword: '+keyword)
   for (var y = 0; y < collected.length; y++) {
     var objecter= collected[y][1]
     if (objecter.display_name.search(keyword) > -1) {
       var yabloc = objecter.locations
       var counteer = false
       for (var x = 0; x < yabloc.length; x++) {
-        if (searchX < yabloc[x][0] && searchX + size > yabloc[x][0] 
-            && searchY < yabloc[x][1] && searchY + size > yabloc[x][1]) {
+        if (searchX < yabloc[x][0] && searchX + size > yabloc[x][0] && searchY < yabloc[x][1] && searchY + size > yabloc[x][1]) {
           if (counteer == false) {
             console.log(objecter.display_name)
-            results.innerHTML += objecter.display_name
+            results.appendChild(document.createTextNode(objecter.display_name))
           }
-          console.log('object found')
           console.log(yabloc[x]);
-          results.innerHTML += yabloc[x]
+          results.appendChild(document.createTextNode(' ' + yabloc[x]);
           counteer = true
-        } else {
-         console.log(objecter.display_name + 'not found')
         }
       }
     }
